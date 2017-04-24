764d149283b2bba614cfbaa270a896f4ed195b4c
ITP312 <itp312@l632-53.local>
2017-04-24 09:54:14 +0800

    Initial Commit

---
+++ P3_TabBarApp/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  P3_TabBarApp
+//
+//  Created by ITP312 on 24/4/17.
+//  Copyright © 2017 ITP312. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
